0245594e9d89b1b80eac7be5e7eba65da5b19fef
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2025-04-15 11:08:38 +0530

    Update BubbleSortAlgorithm.java

--- BubbleSort/BubbleSortAlgorithm.java
+++ BubbleSort/BubbleSortAlgorithm.java
@@ -1,22 +1,30 @@
 import java.util.*;
 
 public class BubbleSortAlgorithm {
-	public static void main(String[] args) {
-		int [] arr= {10,5,3,16,11};
-		bubbleSort(arr);
-		System.out.println(Arrays.toString(arr));
-	}
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 3, 16, 11};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-	public static void bubbleSort(int [] arr) {
+    public static void bubbleSort(int[] arr) {
+        boolean swapped;
 
-		for(int i=0; i<arr.length; i++) {
-			for(int j=1; j<arr.length-i; j++) {
-				if (arr[j]<arr[j-1]) {
-					int temp=arr[j];
-					arr[j]=arr[j-1];
-					arr[j-1]=temp;
-				}
-			}
-		}
-	}
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
