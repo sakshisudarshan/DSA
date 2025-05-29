813185159defa9c124d17389da73d9b913e8be6e
Sakshi Sudarshan <144573279+sakshisudarshan@users.noreply.github.com>
2025-05-29 10:48:34 +0530

    Create PrintOneToN.java

---
+++ RecursionBasics/PrintOneToN.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		oneton(5);
+	}
+	
+	static void oneton(int n){
+	    if(n==0){
+	        return;
+	    }
+	    oneton(n-1);
+	    System.out.println(n);
+	}
+}
